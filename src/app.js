2a52e69052336c664c58d372305db223ae8d3c29
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-13 15:15:20 +0000

    basic random card logic

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const SUITS = ["♦", "♥", "♠", "♣"];
+const VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suitTexts = document.querySelectorAll(".suit");
+  const valueText = document.querySelector(".value");
+
+  let randomSuit = SUITS[Math.floor(Math.random() * SUITS.length)];
+  let color = randomSuit === "♠" || randomSuit === "♣" ? "black" : "red";
+
+  suitTexts.forEach(function(text) {
+    text.textContent = randomSuit;
+    text.style.color = color;
+  });
+
+  valueText.textContent = VALUES[Math.floor(Math.random() * VALUES.length)];
+  valueText.style.color = color;
 };
