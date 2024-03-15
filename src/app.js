b8289e641e13137016b70da9806afcc0f28b32c3
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-15 20:28:25 +0000

    added sliders

--- src/app.js
+++ src/app.js
@@ -5,15 +5,27 @@ const SUITS = ["♦", "♥", "♠", "♣"];
 const VALUES = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 let intervalTime = 10000;
+let interval;
+
+const CARD = document.querySelector(".card");
+const INITIAL_WIDTH = CARD.offsetWidth;
+const INITIAL_HEIGHT = CARD.offsetHeight;
+
+const WIDTH_SLIDER = document.querySelector("#width-range");
+const HEIGHT_SLIDER = document.querySelector("#height-range");
 
 generateRandomCard();
-
-let interval = setInterval(generateRandomCard, intervalTime);
+startInterval();
+setSliderValues(WIDTH_SLIDER, INITIAL_WIDTH, INITIAL_WIDTH * 5);
+setSliderValues(HEIGHT_SLIDER, INITIAL_HEIGHT, INITIAL_HEIGHT * 5);
 
 document.querySelector("#btn").onclick = function() {
   generateRandomCard();
   resetInterval();
 };
+
+WIDTH_SLIDER.addEventListener("change", updateWidth());
+HEIGHT_SLIDER.addEventListener("change", updateHeight());
 
 function generateRandomCard() {
   const suitTexts = document.querySelectorAll(".suit");
@@ -39,3 +51,12 @@ function resetInterval() {
   clearInterval(interval);
   startInterval();
 }
+
+function setSliderValues(slider, min, max) {
+  slider.min = min;
+  slider.max = max;
+}
+
+function updateWidth() {}
+
+function updateHeight() {}
