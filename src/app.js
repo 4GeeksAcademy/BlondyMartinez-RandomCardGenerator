f5d9130346df6968adb4a94d1036a04904c2efa1
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-15 17:25:56 +0000

    removed windows.onload and changed 1 to A in VALUES

--- src/app.js
+++ src/app.js
@@ -2,18 +2,16 @@
 import "./style.css";
 
 const SUITS = ["♦", "♥", "♠", "♣"];
-const VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const VALUES = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 let interval = 10000;
 
-window.onload = function() {
+generateRandomCard();
+
+setInterval(generateRandomCard, interval);
+
+document.querySelector("#btn").onclick = function() {
   generateRandomCard();
-
-  setInterval(generateRandomCard, interval);
-
-  document.querySelector("#btn").onclick = function() {
-    generateRandomCard();
-  };
 };
 
 function generateRandomCard() {
@@ -30,6 +28,4 @@ function generateRandomCard() {
 
   valueText.textContent = VALUES[Math.floor(Math.random() * VALUES.length)];
   valueText.style.color = color;
-
-  interval = 10000;
 }
